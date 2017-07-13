5e966955af7efca49780c2a00b595949dd761475
Kevin Elliott <kevin@welikeinc.com>
2017-07-13 08:13:22 -0700

    Add Visual Studio Code's URI assigned to vscode/vsc editor keywords.

--- lib/better_errors.rb
+++ lib/better_errors.rb
@@ -17,6 +17,7 @@ module BetterErrors
     { symbols: [:macvim, :mvim],        sniff: /vim/i,   url: proc { |file, line| "mvim://open?url=file://#{file}&line=#{line}" } },
     { symbols: [:sublime, :subl, :st],  sniff: /subl/i,  url: "subl://open?url=file://%{file}&line=%{line}" },
     { symbols: [:textmate, :txmt, :tm], sniff: /mate/i,  url: "txmt://open?url=file://%{file}&line=%{line}" },
+    { symbols: [:vscode, :vsc],         sniff: /vsc/i,   url: "vscode://%{file}:%{line}" },
   ]
 
   class << self
